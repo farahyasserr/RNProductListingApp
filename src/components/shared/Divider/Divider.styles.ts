4369c5bedf8bd890ade3fd1fa25a3e09305e78f2
farahyasserr <farah.dawoudd@gmail.com>
2024-02-16 11:03:03 +0400

    feat: Create Divider shared component

---
+++ src/components/shared/Divider/Divider.styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../../theme/Colors";
+
+const styles = StyleSheet.create({
+    divider: {
+      height: 1,
+      backgroundColor: colors.lightGray, 
+      marginVertical: 10,
+    },
+  });
+
+  export default styles
---
+++ src/components/shared/Divider/Divider.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import styles from './Divider.styles';
+
+const Divider = () => {
+  return <View style={styles.divider} />;
+};
+
+
+export default Divider;
