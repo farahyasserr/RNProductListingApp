c2cfa42a39f409e49fe15cc1b08d055630282b12
farahyasserr <farah.dawoudd@gmail.com>
2024-02-16 11:12:12 +0400

    feat(store): Add store provider

--- App.tsx
+++ App.tsx
@@ -3,13 +3,18 @@
 import React from 'react';
 import TabNavigator from './src/navigation/Navigation';
 import { NavigationContainer } from '@react-navigation/native';
+import store from './src/store'
+import { Provider } from 'react-redux'
 
 function App(): React.JSX.Element {
 
   return (
-    <NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
         <TabNavigator />
-    </NavigationContainer>
+      </NavigationContainer>
+    </Provider >
+
   );
 }
 
