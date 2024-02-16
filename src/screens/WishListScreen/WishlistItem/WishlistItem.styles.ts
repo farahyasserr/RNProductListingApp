560c3ca9c9a3da56efa0eb1fb584e73fd51a81fa
farahyasserr <farah.dawoudd@gmail.com>
2024-02-16 15:06:02 +0400

    feat(wishlist): Add Wishlist item component

---
+++ src/screens/WishListScreen/WishlistItem/WishlistItem.styles.ts
@@ -0,0 +1,44 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../../theme/Colors";
+
+const styles = StyleSheet.create({
+mainContainer:{ 
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
+    marginBottom: 20 , 
+    backgroundColor: 
+    colors.white, 
+    borderRadius: 10, 
+    padding: 10
+},
+   title: { 
+    fontWeight: 'bold',
+    maxWidth: 130,
+},
+   image:{
+    width: 60,
+    height: 60,
+    borderRadius: 10,
+    marginEnd: 18
+},
+price:{
+    fontWeight: 'bold', 
+    color: colors.gray 
+},
+iconsContainer:{
+    flexDirection: 'row'
+},
+cartIcon:{
+    marginEnd: 10
+},
+imageAndTextContainer:{
+    flexDirection: 'row', 
+    alignItems: 'center' 
+}
+
+
+
+})
+
+export default styles
---
+++ src/screens/WishListScreen/WishlistItem/WishlistItem.view.tsx
@@ -0,0 +1,37 @@
+import { View , Text, Image } from 'react-native'
+import React from 'react'
+import { Heart, ShoppingCart } from 'lucide-react-native'
+import { colors } from '../../../theme/Colors'
+import styles from './WishlistItem.styles'
+import Product from '../../../types/Product'
+
+interface Props {
+    item: Product
+    addToCartHandler: ()=>void
+    removeFromWishlistHandler: (item : Product)=> void
+}
+
+const CURRENCY = "AED";
+
+function WishlistItem(props: Props) {
+
+    const {item, addToCartHandler, removeFromWishlistHandler} = props
+
+    return (
+    <View style={styles.mainContainer}>
+        <View style={styles.imageAndTextContainer}>
+            <Image source={{ uri: item.thumbnail }} style={styles.image} resizeMode={'cover'} />
+            <View>
+                <Text numberOfLines={2} style={styles.title}>{item.title}</Text>
+                <Text style={styles.price}>{item.price} {CURRENCY}</Text>
+            </View>
+        </View>
+            <View style={styles.iconsContainer}>
+                <ShoppingCart onPress={addToCartHandler} size={20} color={colors.black} style={styles.cartIcon} />
+                <Heart fill={colors.primary} onPress={() => removeFromWishlistHandler(item)} size={20} color={colors.primary} />
+            </View>
+    </View>
+    )
+}
+
+export default WishlistItem
